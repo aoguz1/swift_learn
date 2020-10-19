72c0f51bf27b2339e91bb808a388eceb9bf53b4c
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-10-19 13:24:17 +0300

    add text fields

--- BirtdayNoteTaker/ViewController.swift
+++ BirtdayNoteTaker/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var birdayLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birtdayTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey : "name")
+        let storedBirtday = UserDefaults.standard.object(forKey: "birtday")
+        
+
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        
+        if let newBirtday = storedBirtday as? String {
+            birdayLabel.text = newBirtday
+        }
+        
+        
+        
     }
 
-
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birtdayTextField.text, forKey: "birtday")
+        
+        
+        nameLabel.text = "Name : \(nameTextField.text!)"
+        birdayLabel.text = "Birtday: \(birtdayTextField.text!)"
+        
+        
+        
+    }
+    @IBAction func buttonClick(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
