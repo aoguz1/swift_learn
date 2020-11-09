d6409c4624fabcf852c03814ad1226fa0e83d1c6
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-09 19:12:55 +0300

    alert app finish

--- alertApp/alertApp/ViewController.swift
+++ alertApp/alertApp/ViewController.swift
@@ -3,7 +3,6 @@
 //  alertApp
 //
 //  Created by Abdullah Oğuz on 6.11.2020.
-//
 
 import UIKit
 
@@ -13,32 +12,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordAgainTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-       
-    
-        
-        
     }
 
     @IBAction func saveButton(_ sender: Any) {
       
-        let  alert = UIAlertController(title: "Error", message: "Hatalı tıklama yapıldı" , preferredStyle: UIAlertController.Style.alert)
+    
+        
+        
+        if userNameTextField.text == "" {
+            alert(titleInput: "Kullanıcı Adı Boş", messageInfo: "Lütfen kullanıcı adıızı giriniz")
+        } else if passwordTextField.text == ""{
+            alert(titleInput: "Şifre Alanı Boş", messageInfo: "Lütfen şifrenizi giriniz")
+        } else if passwordTextField.text != passwordAgainTextField.text {
+            alert(titleInput: "Şifreler Aynı Değil", messageInfo: "Lütfen girdiğiniz şifreleri kontrol ediniz")
+        }else{
+            alert(titleInput: "Başarlı", messageInfo: "Başarılı bir şekilde giriş yaptınız..")
+        }
+}
+
+    func alert(titleInput : String,  messageInfo : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInfo, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { (UIAlertAction) in
-            print("tappppedd")
+            print("tapped")
         }
         self.present(alert, animated: true, completion: nil)
-        
-       
         alert.addAction(okButton)
-        
-       
         
         // UIAlertController(title: "Burada vereceğimiz mesajı belirtiyoruz", message: "Alert mesaj açıklamsı", preferredStyle: UIAlertController.Style.alert (vereceğimiz alert tipini belirliyoruz))
         // self.present(burada alert nesnesi bekliyor, animated:  //animasyon olup olamaycağını bool olarak belirtiyoruz, completion: işlem olduktan sonra herhangi bir işlem yapılıp yapılmayacağını söylüyoruz)
+        
+        
     }
-    
-}
 
+}
