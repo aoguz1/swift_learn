34cae4dbb5d8a3f64a1da32e8fd3a3e46b8a1f49
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-09 18:31:54 +0300

    musician project finish

---
+++ MusicianClass/MusicianClass/musician.swift
@@ -0,0 +1,8 @@
+//
+//  musician.swift
+//  MusicianClass
+//
+//  Created by Abdullah Oğuz on 5.01.2021.
+//
+
+import Foundation
---
+++ MusicianClass/MusicianClass/super_musician.swift
@@ -0,0 +1,8 @@
+//
+//  super_musician.swift
+//  MusicianClass
+//
+//  Created by Abdullah Oğuz on 9.01.2021.
+//
+
+import Foundation
